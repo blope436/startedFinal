b5dfe3e1de9398c1c441967df80b681f0dd50ba5
Brandon <blopez98@live.com>
2021-11-22 15:42:02 -0600

    MileStone 2

--- src/pages/appointmentInformation.vue
+++ src/pages/appointmentInformation.vue
@@ -5,7 +5,7 @@ import authorize from "../composable/authorize";
 </script>
 <template>
 <div class="flex items-center justify-center text-red-500 text-5xl text-center bg-gray-500">
-<img class="w-40 h-20" src="../assets/appointmentinformationimage.png" alt="Appointment Information Image"/>
+<img class="w-40 h-20" src="/src/assets/Appointmentinformationimage.png" alt="Appointment Information Image"/>
 </div>
 <div class="flex flex-col items-center mx-auto text-red-700">
 <div ><img class="h-60 px-80 py-3" src="/src/assets/appointmentinfo.png" alt="Appointment Information IMG"></div>
